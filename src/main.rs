cfbfa3bb487dbacba2d1fb90e65ff9e985ecda8e
wych <wychwitchcraft@gmail.com>
2022-12-19 22:08:30 -0600

    reformatted??

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-pub fn advent_day_1_1() {
+pub fn advent_get_elf_calories() -> Vec<Vec<i32>> {
     let file_path = "input1.txt";
     let contents = fs::read_to_string(file_path).expect("Should've been able to read file");
     let string_vecs: Vec<&str> = contents.split("\n\n").collect();
@@ -8,7 +8,7 @@ pub fn advent_day_1_1() {
     for block in &string_vecs {
         let mut elf: Vec<i32> = Vec::new();
         for calorie in block.split("\n") {
-            match calorie.parse::<i32>() {
+            match calorie.parse() {
                 Ok(n) => elf.push(n),
                 Err(_) => println!(
                     "Huh! An error? Elf looks like this {:?} last elves looks like this {:?}",
@@ -19,56 +19,33 @@ pub fn advent_day_1_1() {
         }
         elves.push(elf.into());
     }
-
-    let mut elves_total: Vec<i32> = Vec::new();
-
-    for elf in elves {
-        let total: i32 = elf.iter().sum::<i32>();
-        elves_total.push(total)
-    }
-
-    let max_elf_calories = *elves_total.iter().max().unwrap();
-
-    println!("Top elf total: {}", max_elf_calories);
+    elves
 }
 
-pub fn advent_day_1_2() {
-    let file_path = "input1.txt";
-    let contents = fs::read_to_string(file_path).expect("Should've been able to read file");
-    let string_vecs: Vec<&str> = contents.split("\n\n").collect();
-    let mut elves: Vec<Vec<i32>> = Vec::new();
-    for block in &string_vecs {
-        let mut elf: Vec<i32> = Vec::new();
-        for calorie in block.split("\n") {
-            match calorie.parse::<i32>() {
-                Ok(n) => elf.push(n),
-                Err(_) => println!(
-                    "Huh! An error? Elf looks like this {:?} last elves looks like this {:?}",
-                    elf,
-                    elves.last()
-                ),
-            }
-        }
-        elves.push(elf.into());
-    }
+pub fn advent_get_elf_calorie_totals() -> Vec<i32> {
+    let elf_calories = advent_get_elf_calories();
 
-    let mut elves_total: Vec<i32> = Vec::new();
+    let mut elf_calories_totals: Vec<i32> = Vec::new();
 
-    for elf in elves {
+    for elf in elf_calories {
         let total: i32 = elf.iter().sum::<i32>();
-        elves_total.push(total)
+        elf_calories_totals.push(total)
     }
 
-    elves_total.sort();
-    elves_total.reverse();
+    elf_calories_totals.sort();
+    elf_calories_totals.reverse();
 
+    elf_calories_totals
+}
+
+pub fn advent_day_1() {
+    let elf_calorie_totals = advent_get_elf_calorie_totals();
     println!(
         "Top 3 elves total: {}",
-        elves_total[0] + elves_total[1] + elves_total[2]
+        elf_calorie_totals[0] + elf_calorie_totals[1] + elf_calorie_totals[2]
     );
 }
 
 fn main() {
-    advent_day_1_1();
-    advent_day_1_2();
+    advent_day_1();
 }
