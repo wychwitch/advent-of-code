7b32b45cead50ac41de32b42134362ea9c405831
wych <wychwitchcraft@gmail.com>
2022-12-25 00:54:46 -0600

    day 3 done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-use std::fs;
+use std::{
+    collections::{HashMap, HashSet},
+    fs, vec,
+};
+
+fn advent_load_string(file_name: &str) -> Vec<String> {
+    let contents = fs::read_to_string(file_name).expect("Should've been able to read file");
+    let return_value: Vec<&str> = contents.split("\n").collect();
+    return_value.into_iter().map(String::from).collect()
+}
 
 pub fn advent_get_elf_calories() -> Vec<Vec<i32>> {
     let file_path = "input1.txt";
@@ -220,7 +229,153 @@ pub fn advent_day_2() {
     println!("RPS score: {}\n Cheated: {}", day_2_1, day_2_2);
 }
 
+pub fn advent_rucksort_sum() {
+    let lines = advent_load_string("input3.txt");
+    let mut big_items: Vec<char> = ('A'..='Z').collect();
+    let mut all_items: Vec<char> = ('a'..='z').collect();
+    all_items.append(&mut big_items);
+    let mut counter = 1;
+
+    let mut item_value_map = HashMap::<char, i32>::new();
+
+    for item in &all_items {
+        item_value_map.insert(item.to_owned(), counter);
+        //println!("{item}:{counter:?} ");
+        counter += 1;
+    }
+
+    fn split_and_separate(items: &str) -> (Vec<char>, Vec<char>) {
+        let list_len = items.len() / 2;
+        let (half1, half2) = items.split_at(list_len);
+        (half1.chars().collect(), half2.chars().collect())
+    }
+
+    fn group_elves(elves: Vec<String>) -> Vec<(Vec<char>, Vec<char>, Vec<char>)> {
+        let mut all_elves: Vec<(Vec<char>, Vec<char>, Vec<char>)> = Vec::new();
+        for elf_group in elves.chunks(3) {
+            all_elves.push((
+                elf_group[0].chars().collect(),
+                elf_group[1].chars().collect(),
+                elf_group[2].chars().collect(),
+            ))
+        }
+        all_elves
+    }
+
+    fn find_badge(elf_group: (Vec<char>, Vec<char>, Vec<char>)) -> char {
+        fn return_common_val(
+            largest_bag: Vec<char>,
+            other_bags: (Vec<char>, Vec<char>),
+        ) -> Option<char> {
+            for item in largest_bag {
+                if other_bags.0.contains(&item) && other_bags.1.contains(&item) {
+                    return Some(item);
+                }
+            }
+            return None;
+        }
+
+        let (elf1, elf2, elf3) = elf_group;
+        let badge: char;
+
+        if elf1.iter().len() > elf2.iter().len() && elf1.iter().len() > elf3.iter().len() {
+            badge = return_common_val(elf1, (elf2, elf3)).expect("oops");
+        } else if elf2.iter().len() > elf1.iter().len() && elf2.iter().len() > elf3.iter().len() {
+            badge = return_common_val(elf2, (elf1, elf3)).expect("oops");
+        } else if elf3.iter().len() > elf1.iter().len() && elf3.iter().len() > elf2.iter().len() {
+            badge = return_common_val(elf3, (elf1, elf2)).expect("oops");
+        } else {
+            badge = return_common_val(elf1, (elf2, elf3)).expect("oops");
+        }
+        badge
+    }
+
+    fn hashset(data: &[char]) -> HashSet<char> {
+        HashSet::from_iter(data.iter().cloned())
+    }
+
+    let backpack: Vec<(HashSet<char>, HashSet<char>)> = lines
+        .iter()
+        .map(|line| {
+            let (half1, half2) = split_and_separate(line);
+            (hashset(&half1), hashset(&half2))
+        })
+        .collect();
+    let elf_groups = group_elves(lines);
+    let elf_badges: Vec<(char, i32)> = elf_groups
+        .iter()
+        .map(|group| {
+            let badge = find_badge(group.to_owned());
+            (badge, item_value_map[&badge])
+        })
+        .collect();
+
+    let mut duplicate_items: Vec<(char, i32)> = vec![];
+
+    for compartment in backpack {
+        for item in compartment.0 {
+            if compartment.1.contains(&item) {
+                duplicate_items.push((item, item_value_map[&item]));
+            }
+        }
+    }
+    for duplicate_item in &duplicate_items {
+        assert!(duplicate_item.1.to_owned() == item_value_map[&duplicate_item.0]);
+    }
+
+    let sum: Vec<i32> = duplicate_items
+        .iter()
+        .map(|item_value| {
+            let (_, value) = item_value;
+            value.to_owned()
+        })
+        .collect();
+
+    let badge_sum: Vec<i32> = elf_badges
+        .iter()
+        .map(|item_value| {
+            let (_, value) = item_value;
+            value.to_owned()
+        })
+        .collect();
+
+    println!("ITEMS");
+    for item in duplicate_items {
+        println!("{:?}", item);
+    }
+    println!("total: {}", sum.into_iter().sum::<i32>());
+
+    println!("BADGES");
+    for badge in elf_badges {
+        println!("{:?}", badge);
+    }
+    println!("total: {}", badge_sum.into_iter().sum::<i32>());
+}
+
+fn advent_overlap() {
+    let lines = advent_load_string("test.txt");
+    let bound_pairs: Vec<(String, String)> = lines
+        .into_iter()
+        .map(|line| {
+            let line_pair = line.split_once(',').unwrap();
+            (line_pair.0.to_owned(), line_pair.1.to_owned())
+        })
+        .collect();
+    let split_pairs: Vec<((i32, i32), (i32, i32))> = bound_pairs
+        .into_iter()
+        .map(|pair| {
+            let set1 = pair.0.split_once('-').unwrap();
+            let set2 = pair.1.split_once('-').unwrap();
+
+            let set1 = (set1.0.parse().unwrap(), set1.1.parse().unwrap());
+            let set2 = (set2.0.parse().unwrap(), set2.1.parse().unwrap());
+            (set1, set2)
+        })
+        .collect();
+}
+
 fn main() {
     //advent_day_1();
-    advent_day_2();
+    //advent_day_2();
+    //advent_rucksort_sum();
 }
